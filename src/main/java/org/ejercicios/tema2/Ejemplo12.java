bc7c610291804f540a3a70572affbc6557c54e26
Andrés P <spelujo@gmail.com>
2025-11-17 11:15:35 +0100

    Ejercicio

---
+++ src/main/java/org/ejercicios/tema2/Ejemplo12.java
@@ -0,0 +1,66 @@
+package org.ejercicios.tema2;
+
+import java.util.concurrent.Semaphore;
+
+/** @author AndrésPérezM
+ * Ejemplo 12
+ * */
+public class Ejemplo12 {
+    // Main
+    public static void main(String[] args) {
+        Horno horno = new Horno();
+
+        String[] nombres = {"A", "B"};
+
+        for (int i = 0; i < nombres.length; i++) {
+            Panadero panadero = new Panadero("Panadero-" + nombres[i], horno);
+            panadero.start();
+        }
+    }
+
+    // Horno
+    static class Horno {
+        private final int CAPACIDAD = 1;
+
+        private final Semaphore acceso =  new Semaphore(CAPACIDAD);
+
+        // Número de pastales que han horneado
+        private int numPasteles = 0;
+
+        private void usarHorno() {
+            String panadero = Thread.currentThread().getName();
+
+            try {
+                System.out.println("🥣 " + panadero + " 🧑‍🍳 está PREPARANDO la masa. (3 seg)");
+                Thread.sleep(3000);
+
+                System.out.println("⌛ " + panadero + " 🧑‍🍳 está ESPERANDO el horno.");
+                acceso.acquire();
+
+                System.out.println("🔥 " + panadero + " 🧑‍🍳 está HORNEANDO... (7 seg)");
+                Thread.sleep(7000);
+            } catch (InterruptedException e) {
+                System.out.println("Error en el acceso al horno: " + e.getMessage());
+            } finally {
+                numPasteles++;
+                acceso.release();
+                System.out.println("🍞 " + panadero + " 🧑‍🍳 ha TERMINADO de hornear el pastél número " + numPasteles);
+            }
+        }
+    }
+
+    // Panaderos
+    static class Panadero extends Thread {
+        private final Horno horno;
+
+        public Panadero(String nombre, Horno horno) {
+            super(nombre);
+            this.horno = horno;
+        }
+
+        @Override
+        public void run() {
+            horno.usarHorno();
+        }
+    }
+}
