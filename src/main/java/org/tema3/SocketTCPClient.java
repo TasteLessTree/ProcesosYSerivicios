b0300f1242f29087ed78a9ee7754f53a11002bf7
Andrés P <spelujo@gmail.com>
2025-11-24 11:15:52 +0100

    TCP/IP

---
+++ src/main/java/org/tema3/SocketTCPClient.java
@@ -0,0 +1,42 @@
+package org.tema3;
+
+import java.io.*;
+import java.net.Socket;
+
+public class SocketTCPClient {
+    public static void main(String[] args) {
+        // Datos de la conexón
+        String host = "localhost";
+        int port = 42069;
+
+        System.out.println("Esperando conexiones...");
+
+        // Cerrar el socket al final
+        try (Socket socket = new Socket(host, port)) {
+            System.out.println("Conexión establecida.");
+
+            // Obtener flujos
+            OutputStream outputStream = socket.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+            InputStream inputStream = socket.getInputStream();
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+            // Enviar datos al servidor
+            dataOutputStream.writeInt(100);
+            // Aseguramos que sale del buffer
+            dataOutputStream.flush();
+
+            // El cliente espera a que el servidor responda
+            int respuesta = dataInputStream.readInt();
+            System.out.println("Mensaje del servidor: " + respuesta);
+
+            System.out.println("Cerrando conexiones...");
+            // Cerramos la conexión
+            socket.close();
+            System.out.println("Coneción cerrada con éxito");
+        } catch (IOException e) {
+            System.out.println("Error en el servidor: " + e.getMessage());
+        }
+    }
+}
